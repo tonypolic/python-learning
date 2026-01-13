76002f697ef4c97a943173561c2a98b984d7551d
Tony <tonypolic@gmail.com>
2026-01-13 19:14:25 +0200

    printf

--- basics/input_example.py
+++ basics/input_example.py
@@ -5,5 +5,5 @@ print('It is good to meet you, ' + my_name)
 print('The length of your name is:')
 print(len(my_name))
 print('What is your age?')  # Ask for their age.
-my_age = input('>')
-print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+my_age =(input('>'))
+print(f' You will be {int(my_age) + 1}  in a year.')
